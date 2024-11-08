05d5f18d88a535ab24f97c70a5b06ab18d919c2f
amandaolmr <amandinha.g.2009@hotmail.com>
2024-11-08 20:29:14 -0300

    commit do projeto de banco de dados com python

---
+++ usar_bd.py
@@ -0,0 +1,13 @@
+import DAO
+
+#saida = DAO.inserir_usuario("amanda", "email@email.com", "1234")
+#saida = DAO.inserir_usuario("maria", "maria@email.com", "1234")
+
+#saida = DAO.buscar_usuario('amanda')
+#saida = DAO.listar_usuarios()
+
+#saida = DAO.atualizar_usuario(1, "JOAO", "joao@emial.com")
+
+saida = DAO.deletar_usuario(2)
+
+print(saida)
